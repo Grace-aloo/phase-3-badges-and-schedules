905076471ce5e89cf6a4e5f741e238893c5a916c
grace_aloo <graceaaloo@gmail.com>
2023-02-08 11:33:47 +0300

    done

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,35 @@
 # Write your code here.
+
+def badge_maker (name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator (list)
+    list.map do |name|
+         badge_maker(name)
+    end
+end
+
+def assign_rooms speakers
+    room =1
+    speakers.map do |speaker|
+        message = "Hello, #{speaker}! You'll be assigned to room #{room}!"
+        room += 1
+        message
+    end
+end
+
+def printer(speakers)
+    badges = batch_badge_creator(speakers)
+    room_assignments = assign_rooms(speakers)
+
+    badges.each do |badge|
+        puts badge
+    end
+    # puts ""
+    room_assignments.each do |assignment|
+      puts assignment
+    end
+    #output.join('\n')
+end
+
